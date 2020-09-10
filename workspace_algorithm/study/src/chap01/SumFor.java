689b6a941f27bdb7ee8e631db1fef7990c82ee5e
hyeyeon-lee <dear.yeonee@gmail.com>
2020-09-10 22:50:58 +0900

    chap01 반복

---
+++ workspace_algorithm/study/src/chap01/SumFor.java
@@ -0,0 +1,35 @@
+package chap01;
+
+import java.util.Scanner;
+
+// 1, 2, n의 합을 구합니다.
+
+public class SumFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("1부터 n까지의 합을 구합니다.");
+		System.out.println("n의 값:");
+		int n = stdIn.nextInt();
+
+		int sum = 0;
+
+		for (int i = 0; i <= n; i++) {
+			// Q7. 출력 내용 변경하기
+			if (i == 0)
+				continue;
+			sum += i;
+			System.out.print(i);
+			if (i == n)
+				System.out.print(" = ");
+			else
+				System.out.print(" + ");
+		}
+		System.out.print(sum);
+		System.out.println();
+		System.out.println("1부터 " + n + "까지의 합은 " + sum);
+	}
+
+}
---
+++ workspace_algorithm/study/src/chap01/SumWhile.java
@@ -0,0 +1,29 @@
+package chap01;
+
+import java.util.Scanner;
+
+// 1, 2, n까지의 합을 구합니다.
+public class SumWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("1부터 n까지의 합을 구합니다.");
+		System.out.println("n의 값:");
+		int n = stdIn.nextInt();
+
+		int sum = 0;
+		int i = 1;
+
+		while(i<=n) { // i가 n이하면 반복
+			sum+=i; // sum = sum + i
+			i++; // i 1증가
+		}
+		// Q6. 최종 i의 값이 n+1
+		System.out.println(i);
+		System.out.println("1부터 " + n + "까지의 합은 " + sum);
+	}
+
+}
