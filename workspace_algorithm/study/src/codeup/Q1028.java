552d78011af38f8826f95e72a6740e7c95fe5ac4
hyeyeon-lee <dear.yeonee@gmail.com>
2020-09-27 22:44:34 +0900

    문제 풀이
    codeup
    -  [기초-출력] 1028

---
+++ workspace_algorithm/study/src/codeup/Q1028.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1028 {
+
+	// 1028 : [기초-데이터형] 정수 1개 입력받아 그대로 출력하기2(설명)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		long num = s.nextLong();
+		System.out.println(num);
+	}
+
+}
