007599e53a3dbadeb0189525a7efbffcec608206
hyeyeon-lee <dear.yeonee@gmail.com>
2020-10-11 23:37:02 +0900

    문제 풀이
    codeup
    -  [기초-논리연산] 1059~1061
    java
    - chap02. Int2DArray

---
+++ workspace_algorithm/study/src/codeup/Q1059.java
@@ -0,0 +1,17 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1059 {
+
+	// 1059 : [기초-비트단위논리연산] 비트단위로 NOT 하여 출력하기(설명)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		int num = s.nextInt();
+
+		System.out.printf("%d", ~num);
+	}
+
+}
---
+++ workspace_algorithm/study/src/codeup/Q1061.java
@@ -0,0 +1,18 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1061 {
+
+	// 1061 : [기초-비트단위논리연산] 비트단위로 OR 하여 출력하기(설명)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+
+		System.out.printf("%d", a | b);
+	}
+
+}
