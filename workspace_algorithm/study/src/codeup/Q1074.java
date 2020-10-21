0b2be39ee349d707dfdbe7017e580cb514225b37
hyeyeon-lee <dear.yeonee@gmail.com>
2020-10-21 20:49:27 +0900

    문제 풀이
    codeup
    -  [기초-반복실행구조] 1074~1075
    java
    - chap02. PhysicalExamination (class)

---
+++ workspace_algorithm/study/src/codeup/Q1074.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1074 {
+	// 1074 : [기초-반복실행구조] 정수 1개 입력받아 카운트다운 출력하기1(설명)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		while (num != 0) {
+			System.out.println(num--);
+		}
+
+	}
+}
