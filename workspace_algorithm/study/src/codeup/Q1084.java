8512e98ab74c18c29dfb074f2dbd3c100cc5bd7a
hyeyeon-lee <dear.yeonee@gmail.com>
2020-10-30 23:19:56 +0900

    문제 풀이
    codeup
    -  [기초-종합] 1084

---
+++ workspace_algorithm/study/src/codeup/Q1084.java
@@ -0,0 +1,27 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1084 {
+	// 1084 : [기초-종합] 빛 섞어 색 만들기(설명)
+	public static void main(String[] args) {
+
+		int red, green, blue;
+		Scanner s = new Scanner(System.in);
+		red = s.nextInt();
+		green = s.nextInt();
+		blue = s.nextInt();
+
+		int cnt = 0;
+
+		for (int i = 0; i < red; i++)
+			for (int j = 0; j < green; j++)
+				for (int k = 0; k < blue; k++) {
+					System.out.println(i + " " + j + " " + k);
+					cnt++;
+				}
+
+		System.out.println(cnt);
+
+	}
+}
