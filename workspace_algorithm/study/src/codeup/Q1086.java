595c9d17e90df85f12b955e05d61bbc0be4b1d82
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-03 22:16:01 +0900

    문제 풀이
    codeup
    -  [기초-종합] 1086

--- workspace_algorithm/study/src/codeup/Q1086.java
+++ workspace_algorithm/study/src/codeup/Q1086.java
@@ -2,19 +2,18 @@ package codeup;
 
 import java.util.Scanner;
 
-public class Q1085 {
-	// 1085 : [기초-종합] 소리 파일 저장용량 계산하기(설명)
+public class Q1086 {
+	// 1086 : [기초-종합] 그림 파일 저장용량 계산하기(설명)
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
 		int h = sc.nextInt();
 		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int s = sc.nextInt();
 
-		long bit = (long) h * b * c * s;
+		long bit = (long) w * h * b;
 		double mb = (double) bit / 8 / 1024 / 1024;
-		System.out.printf("%.1f MB", mb);
+		System.out.printf("%.2f MB", mb);
 
 	}
 }
