8b1bceae84e6d5ca245aa43e3f02e2851bef5dd0
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-22 23:40:51 +0900

    문제 풀이
    codeup
    - 1106 : int의 범위 (2020.11.22)
    - 1107 : 반복 출력하기 1 (2020.11.22)
    java
    - Quest1(SeqSearchSen 변형)

---
+++ workspace_algorithm/study/src/codeup/Q1106.java
@@ -0,0 +1,12 @@
+package codeup;
+
+public class Q1106 {
+	// 1106 : int의 범위
+	public static void main(String[] args) {
+		Integer min = Integer.MIN_VALUE;
+		Integer max = Integer.MAX_VALUE;
+
+		System.out.printf("%d ", min);
+		System.out.printf("%d", max);
+	}
+}
---
+++ workspace_algorithm/study/src/codeup/Q1107.java
@@ -0,0 +1,12 @@
+package codeup;
+
+public class Q1107 {
+	// 1107 : 반복 출력하기 1
+	public static void main(String[] args) {
+		final int COUNT = 50;
+
+		for(int i = 0; i < COUNT; i++) {
+			System.out.print("print");
+		}
+	}
+}
