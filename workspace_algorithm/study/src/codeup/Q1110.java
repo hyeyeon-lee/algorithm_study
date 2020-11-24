68ffeff1d1d55740611f7e5da44fb3b227e04208
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-24 20:42:12 +0900

    문제 풀이
    codeup
    - 1110 : 정수 그대로 출력하기 (2020.11.24)

---
+++ workspace_algorithm/study/src/codeup/Q1110.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1110 {
+	// 1110 : 정수 그대로 출력하기
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		System.out.printf("%d", a);
+
+	}
+}
