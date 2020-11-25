dd3e32e9809b3630853d1badcce447e378faa4be
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-25 22:03:45 +0900

    문제 풀이
    codeup
    - 1111 : %출력 (2020.11.25)
    - 1112 : 두 정수 출력 (2020.11.25)

---
+++ workspace_algorithm/study/src/codeup/Q1111.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1111 {
+	// 1111 : %출력
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		System.out.printf("%d%%", a);
+
+	}
+}
---
+++ workspace_algorithm/study/src/codeup/Q1112.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1112 {
+	// 1111 : %출력
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.printf("%d %d", a, b);
+
+	}
+}
