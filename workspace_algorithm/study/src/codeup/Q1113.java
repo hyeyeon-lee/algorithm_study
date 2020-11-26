af5530d6a49196a47aa62df4fd5432b8f67a4004
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-26 22:21:47 +0900

    문제 풀이
    codeup
    - 1113 : 바꿔서 출력하기

--- workspace_algorithm/study/src/codeup/Q1113.java
+++ workspace_algorithm/study/src/codeup/Q1113.java
@@ -2,14 +2,14 @@ package codeup;
 
 import java.util.Scanner;
 
-public class Q1112 {
-	// 1112 : 두 정수 출력
+public class Q1113 {
+	// 1113 : 바꿔서 출력하기
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 
 		int a = s.nextInt();
 		int b = s.nextInt();
-		System.out.printf("%d %d", a, b);
+		System.out.printf("%d %d", b, a);
 
 	}
 }
