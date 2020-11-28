e2a1b60221535f67bdf491a364ad0b5cd2e3abd2
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-28 22:32:22 +0900

    문제 풀이
    codeup
    - 1114 : 두 정수의 덧셈

---
+++ workspace_algorithm/study/src/codeup/Q1114.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1114 {
+	// 1114 : 두 정수의 덧셈
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.printf("%d", a + b);
+
+	}
+}
