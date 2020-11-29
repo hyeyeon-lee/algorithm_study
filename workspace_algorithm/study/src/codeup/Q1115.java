cc79c2f5b5f5a4d2c46c8ac22372b8ce642a54fd
hyeyeon-lee <dear.yeonee@gmail.com>
2020-11-29 21:36:46 +0900

    문제 풀이
    codeup
    - 1115 : 두 정수의 덧셈 (64비트)

---
+++ workspace_algorithm/study/src/codeup/Q1115.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1115 {
+	// 1115 : 두 정수의 덧셈 (64비트)
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		long a = s.nextLong();
+		long b = s.nextLong();
+		System.out.printf("%d", a + b);
+
+	}
+}
