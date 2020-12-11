01f0ee56e2463bd70e6eab6e261653c072b2cfdd
hyeyeon-lee <dear.yeonee@gmail.com>
2020-12-11 22:49:18 +0900

    문제 풀이
    codeup
    - 1128 : n \* 123456789

---
+++ workspace_algorithm/study/src/codeup/Q1128.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Q1128 {
+	// 1128 : n * 123456789
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int num = s.nextInt();
+		System.out.print(num * 123456789L);
+
+	}
+}
