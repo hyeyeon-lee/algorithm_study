baa3ce10c0a34310771ac259cfe339a728aa6f8c
hyeyeon-lee <dear.yeonee@gmail.com>
2020-12-12 22:48:34 +0900

    문제 풀이
    codeup
    - 1131 : 문자 출력하기
    - 1132 : 문자열 출력하기

---
+++ workspace_algorithm/study/src/codeup1/Q1132.java
@@ -0,0 +1,14 @@
+package codeup1;
+
+import java.util.Scanner;
+
+public class Q1132 {
+	// 1132 : 문자열 출력하기
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		String str = s.nextLine();
+		System.out.printf("%s", str);
+
+	}
+}
