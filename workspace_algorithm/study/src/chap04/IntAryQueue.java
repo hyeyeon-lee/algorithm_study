ec149931285e1c0b6381c1dc566de43f0d72ba40
hyeyeon-lee <dear.yeonee@gmail.com>
2021-02-04 21:19:38 +0900

    문제 풀이
    java
    - IntAryQueue Exception

--- workspace_algorithm/study/src/chap04/IntAryQueue.java
+++ workspace_algorithm/study/src/chap04/IntAryQueue.java
@@ -10,24 +10,57 @@ public class IntAryQueue {
 	private int num; // 현재 데이터 수
 	private int[] que; // 큐 본체
 
+	// 큐가 비었을 때
+	public class EmptyIntAryQueueException extends RuntimeException {
+		public EmptyIntAryQueueException() {
+
+		}
+	}
+
+	// 큐가 가득 찼을 때
+	public class OverflowIntAryQueueException extends RuntimeException {
+		public OverflowIntAryQueueException() {
+
+		}
+	}
+
+	// 생성자
 	public IntAryQueue(int capacity) {
 		max = capacity;
 		num = 0;
-		que = new int[max];
+		try {
+			que = new int[max];
+		} catch (OutOfMemoryError e) {
+			max = 0;
+		}
 	}
 
 	// 큐에 데이터를 넣음 (인큐)
-	public int enque(int x) {
+	public int enque(int x) throws OverflowIntAryQueueException {
+		if (num >= max)
+			throw new OverflowIntAryQueueException();
 		que[num++] = x;
 		return x;
 	}
 
 	// 큐에서 데이터를 뺌 (디큐)
-	public int deque() {
+	public int deque() throws EmptyIntAryQueueException {
+		if (num <= 0)
+			throw new EmptyIntAryQueueException();
 		int x = que[0];
 		for (int i = 0; i < num; i++)
 			que[i] = que[i + 1];
 		num--;
+		return x;
+	}
+
+	// 큐의 프론트 데이터 구하기
+	public int peek() throws EmptyIntAryQueueException {
+		int x = 0;
+		if (num <= 0)
+			throw new EmptyIntAryQueueException();
+		x = que[0];
+
 		return x;
 	}
 
