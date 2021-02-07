66f07845694df4f8fb22ca04a0862478a6438416
hyeyeon-lee <dear.yeonee@gmail.com>
2021-02-07 22:22:18 +0900

    문제 풀이
    codeup
    - 1714

--- workspace_algorithm/study/src/codeup1/Q1714.java
+++ workspace_algorithm/study/src/codeup1/Q1714.java
@@ -2,21 +2,16 @@ package codeup1;
 
 import java.util.Scanner;
 
-public class Q1402 {
-	// 1402 : 거꾸로 출력하기 3
+public class Q1714 {
+	// 1714 : 숫자 거꾸로 출력하기
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 
-		int n = s.nextInt();
-		int[] arr = new int[n];
-
-		for (int i = 0; i < n; i++) {
-			arr[i] = s.nextInt();
-		}
-
+		String num = s.nextLine();
+		String[] arr = num.split("");
 		for (int i = arr.length - 1; i >= 0; i--)
-			System.out.printf("%d ", arr[i]);
+			System.out.printf("%s", arr[i]);
 	}
 
 }
