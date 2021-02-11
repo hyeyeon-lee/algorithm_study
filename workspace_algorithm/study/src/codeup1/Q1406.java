4f78e50d538b54d37c839db696b6f1982f8376ab
hyeyeon-lee <dear.yeonee@gmail.com>
2021-02-11 22:04:09 +0900

    문제 풀이
    codeup
    - 1406

---
+++ workspace_algorithm/study/src/codeup1/Q1406.java
@@ -0,0 +1,18 @@
+package codeup1;
+
+import java.util.Scanner;
+
+public class Q1406 {
+	// 1406 : love
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		String text = s.next();
+
+		if (text.equals("love"))
+			System.out.println("I love you.");
+
+	}
+
+}
