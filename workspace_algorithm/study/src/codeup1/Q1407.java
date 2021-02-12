206153445fe178d1b99548f340ba1ebc9df40989
hyeyeon-lee <dear.yeonee@gmail.com>
2021-02-12 21:44:22 +0900

    문제 풀이
    codeup
    - 1407

---
+++ workspace_algorithm/study/src/codeup1/Q1407.java
@@ -0,0 +1,18 @@
+package codeup1;
+
+import java.util.Scanner;
+
+public class Q1407 {
+	// 1407 : 문자열 출력하기 1
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+		String text = s.nextLine();
+
+		System.out.println(text.replaceAll("\\s", ""));
+
+
+	}
+
+}
