ba1732d934bdbb92cc54c5af3090461bc3d843d1
hyeyeon-lee <dear.yeonee@gmail.com>
2021-03-02 21:31:12 +0900

    문제 풀이
    codeup
    - 1297

---
+++ workspace_algorithm/study/src/codeup1/Q1297.java
@@ -0,0 +1,26 @@
+package codeup1;
+
+import java.util.Scanner;
+
+public class Q1296 {
+	// 1297 : 단면의 최대 넓이
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int max = 0;
+        int b = 0;
+
+        for(int i = 1; i <= n/2 ; i++) {
+            int w = (n-2*i)*i;
+            if(w > max) {
+                max = w;
+                b = i;
+            }
+        }
+        
+        System.out.println(b);
+
+
+	}
+
+}
