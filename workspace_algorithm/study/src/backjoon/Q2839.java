3b80013745728156915f0906f0fbf881b3983f14
hyeyeon-lee <dear.yeonee@gmail.com>
2021-03-15 22:54:35 +0900

    문제 풀이
    backjoon
    - 2839

--- workspace_algorithm/study/src/backjoon/Q2839.java
+++ workspace_algorithm/study/src/backjoon/Q2839.java
@@ -12,19 +12,19 @@ public class Q2839 {
 		int n = stdIn.nextInt(); // 설탕 무게 입력
 		int cnt = 0; // 봉지 개수
 
-		cnt += n / 5;
-		n %= 5;
+		while (true) {
+			if (n == 4 || n == 7) {
+				cnt = -1;
+				break;
+			}
+			if (n % 5 == 0) cnt = n / 5;
+			else if (n % 5 == 1 || n % 5 == 3) cnt = n / 5 + 1;
+			else if (n % 5 == 2 || n % 5 == 4) cnt = n / 5 + 2;
 
-		/*
-		 * if (n != 0 && cnt != 0 && (n + 5) % 3 == 0) { // 무게가 남았다면 5kg 하나 해체
-		 * cnt--; n += 5; }
-		 */
-		cnt += n / 3;
-		n %= 3;
+			break;
+		}
 
-		if (n != 0) cnt = -1;
-
-		System.out.println(cnt);
+		System.out.print(cnt);
 	}
 
 }
