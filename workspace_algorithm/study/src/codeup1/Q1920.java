c33fc87296a50ee7b52a24de215f21d3451463db
hyeyeon-lee <dear.yeonee@gmail.com>
2021-05-18 23:58:41 +0900

    문제 풀이
    codeup
    - 1920

--- workspace_algorithm/study/src/codeup1/Q1920.java
+++ workspace_algorithm/study/src/codeup1/Q1920.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class Q1920 {
 	// 1920 : (재귀함수) 2진수 변환
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 
 		int dec = s.nextInt();
-		System.out.println(toBinary(dec));
+		toBinary(dec);
 
 	}
 
-	public static int toBinary(int dec) {
-		int result = 0;
-
-		//
-
-		return result;
+	public static void toBinary(int dec) {
+		dec /= 2;
+		if (dec != 0) {
+			System.out.print(dec % 2);
+			toBinary(dec);
+		} else System.out.print(dec);
 
 	}
 
