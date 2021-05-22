7ab180530469af6cecff12961fdfc6281b71fd83
hyeyeon-lee <dear.yeonee@gmail.com>
2021-05-22 21:58:10 +0900

    문제 풀이
    backjoon
    - 15552

--- workspace_algorithm/study/src/backjoon/Q15552.java
+++ workspace_algorithm/study/src/backjoon/Q15552.java
@@ -1,21 +1,38 @@
 package backjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Q15552 {
 
 	// 15552 빠른 A+B
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Scanner stdIn = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int t = stdIn.nextInt(); // 총 테스트케이스
+		int t = Integer.parseInt(br.readLine());// 총 테스트케이스
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
 
 		for (int i = 0; i < t; i++) {
-			int a = stdIn.nextInt();
-			int b = stdIn.nextInt();
-			System.out.println(a + b);
+			st = new StringTokenizer(br.readLine());
+
+			int[] arr = new int[2];
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+			bw.write(String.valueOf(arr[0] + arr[1])); // string.valueOf int ->
+														// string 출력
+			bw.write("\n");
 		}
+		bw.flush();
 
 	}
 
