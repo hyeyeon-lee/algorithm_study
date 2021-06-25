0c5041290743c5b1b88853f36ade183b5f79b677
hyeyeon-lee <dear.yeonee@gmail.com>
2021-06-25 23:32:13 +0900

    문제 풀이
    codeup
    - Q1420 문제 오류 맞게 수정

--- workspace_algorithm/study/src/codeup1/Q1420.java
+++ workspace_algorithm/study/src/codeup1/Q1420.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Q1420 {
 	// 1420 : 3등 찾기
@@ -16,22 +17,25 @@ public class Q1420 {
 		Scanner s = new Scanner(System.in);
 
 		int n = s.nextInt();
+		int[] arr = new int[n];
 
 		// 친구들의 성적은 모두 다르다 전제로 키 설정
-		Map<Integer, String> map = new HashMap<Integer, String>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
 
 		for (int i = 0; i < n; i++) {
 			String name = s.next();
 			int score = s.nextInt();
-			map.put(score, name);
+			map.put(name, score);
+			arr[i] = score;
 		}
 
-		Object[] mapKeys = map.keySet().toArray();
-		Arrays.sort(mapKeys);
+		// 정렬 후 세번째 찾기
+		Arrays.sort(arr);
+		int thirdScore = arr[n - third];
 
-		// 전체 인원 - 3
-		String student = map.get(mapKeys[n - third]);
-		System.out.println(student);
+		for (String key : map.keySet())
+			if (map.get(key) == thirdScore) System.out.println(key);
+
 	}
 
 }
