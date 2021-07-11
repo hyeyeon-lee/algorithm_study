510383cd2652694573e4189b9d4f844e1b72af4f
hyeyeon-lee <dear.yeonee@gmail.com>
2021-07-11 21:52:39 +0900

    문제 풀이
    codeup
    - 1443

--- workspace_algorithm/study/src/codeup1/Q1443.java
+++ workspace_algorithm/study/src/codeup1/Q1443.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 
-public class Main {
-	// 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+public class Q1443 {
+    // 1443 : 삽입 정렬
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
         int n = s.nextInt();
         int[] arr = new int[n];
-        
-        for(int i=0; i<n; i++){
+
+        for (int i = 0; i < n; i++) {
             arr[i] = s.nextInt();
         }
-        
+
         int key;
         int j;
-        
-        for(int i=1; i<n; i++){
+
+        for (int i = 1; i < n; i++) {
             key = arr[i];
-            
-            for(j = i+1; j > 0 && arr[j]> key; j--) {
-              arr[j+1] = arr[j];
-            }
-            arr[j+1] = key;
+
+            for (j = i; j > 0 && arr[j - 1] > key; j--)
+                arr[j] = arr[j - 1];
+            arr[j] = key;
         }
-        
-        for (int i= 0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
-        
-
-
-	}
+    }
 }
